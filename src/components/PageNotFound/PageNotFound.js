d2b8a3587042c51887c4bcfbc07f4e1e4f65c591
PascalPromoTrinity <lapascal89@gmail.com>
2021-11-20 15:18:19 +0100

    page not found updated

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
+
+import "./PageNotFound.scss";
 
 const PageNotFound = () => {
   return (
-    <div>
-      PageNotFound
+    <div className="not-found">
+      <h1>404</h1>
+      <h1>Page Not Found</h1>
     </div>
   );
 };
